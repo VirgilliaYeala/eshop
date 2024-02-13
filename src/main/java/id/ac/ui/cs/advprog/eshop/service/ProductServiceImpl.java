0b5d15995a4ff18ef8276da9fd4ba4acf749701c
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-02-13 23:27:50 +0700

    fix: remove unnecessary field injection ProductServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -4,7 +4,6 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.util.ArrayList;
@@ -16,9 +15,11 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
 
-  @Autowired
   private ProductRepository productRepository;
 
+  public ProductServiceImpl(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
   @Override
   public Product create(Product product) {
