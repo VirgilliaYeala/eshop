8c55e50959e060f9acd80c7c81201acf2a16a958
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-02-24 13:15:03 +0700

    refactor: reorganize Car & Product Repository to meet OCP standards

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryImpl.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+  private List<Product> productData = new ArrayList<>();
+
+  public Product create(Product product) {
+    productData.add(product);
+    return product;
+  }
+
+  public Iterator<Product> findAll() {
+    return productData.iterator();
+  }
+
+  public Product edit(Product editedProduct) {
+    String editedProductId = editedProduct.getProductId();
+    int editedProductQuantity = editedProduct.getProductQuantity();
+
+    if (editedProductQuantity <= 0) editedProduct.setProductQuantity(0);
+
+    Product productInRepository = this.findById(editedProductId);
+    int indexEditedProduct = productData.indexOf(productInRepository);
+    productData.set(indexEditedProduct, editedProduct);
+    return editedProduct;
+  }
+  public Product deleteById(String productId) {
+    Product deletedProduct = this.findById(productId);
+    productData.remove(deletedProduct);
+    return deletedProduct;
+  }
+
+  public Product findById(String findProductId) {
+    Iterator<Product> productIterator = this.findAll();
+    while (productIterator.hasNext()) {
+      Product dataProduct = productIterator.next();
+      if (dataProduct.getProductId().equals(findProductId)) {
+        return dataProduct;
+      }
+    }
+    return null;
+  }
+}
