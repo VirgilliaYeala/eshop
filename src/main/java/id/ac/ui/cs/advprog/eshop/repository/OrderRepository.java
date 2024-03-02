833670cdae82f9381c1f6fb8b99259cf4375f88f
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-03-02 23:37:45 +0700

    feat: [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -8,9 +8,36 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
     
-    public Order save(Order order) {return null;}
+    public Order save(Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getOrderId().equals(order.getOrderId())) {
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i += 1;
+        }
 
-    public Order findById(String orderId) {return null;}
+        orderData.add(order);
+        return order;
+    }
 
-    public List<Order> findAllByAuthor(String author) {return null;}
+    public Order findById(String orderId) {
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getOrderId().equals(orderId)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);            }
+        }
+        return result;
+    }
 }
