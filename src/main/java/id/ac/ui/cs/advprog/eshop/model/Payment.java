8f1e4da4bf3500f4c6ce791a34b87ee11a9ff790
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-03-06 13:13:03 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
-@Builder
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.StatusPayment;
+
 @Getter
 public class Payment {
     private String paymentId;
@@ -13,4 +14,43 @@ public class Payment {
     private Long orderTime;
     private Map<String, String> paymentData;
     private Order paymentOrder;
+    private String statusPayment;
+
+    public Payment(String paymentId, String paymentMethod, Order paymentOrder, Map<String, String> paymentData, String statusPayment) {
+        this.paymentId = paymentId;
+        this.paymentMethod = paymentMethod;
+        this.setOrder(paymentOrder);
+        this.setPaymentData(paymentData);
+        this.setStatus(statusPayment);
+    }
+
+    public Payment(String paymentId, String paymentMethod, Order order, Map<String, String> paymentData) {
+        this(paymentId, paymentMethod, order, paymentData, StatusPayment.PENDING.getValue());
+    }
+
+    public void setStatus(String statusPayment) {
+        if (!StatusPayment.contains(statusPayment)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+
+        this.statusPayment = statusPayment;
+    }
+
+    private void setOrder(Order paymentOrder) {
+        if (paymentOrder == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        this.paymentOrder = paymentOrder;
+    }
+
+    protected void setPaymentData(Map<String, String> paymentData) {
+        if (PaymentMethod.contains(this.paymentMethod)) {
+            throw new IllegalArgumentException(
+                "Cannot set method-specific payment data for non-method-specific payment"
+            );
+        }
+
+        this.paymentData = null;
+    }
 }
